984a601ef2c5d6252d49604e427a8aa10e1d4d22
MANISH <khandagalemanish21@gmail.com>
2025-01-02 18:02:42 +0530

    redepoly

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       <BrowserRouter>
       <Navbar/>
       <Routes>
-        <Route path='/' element={<Shop/>}/>
+        <Route path='/SHOPPER' element={<Shop/>}/>
         <Route path='/mens' element={<ShopCategory banner={men_banner} category='men' />}/>
         <Route path='/kids' element={<ShopCategory banner={kid_banner}  category='kid'/>}/>
         <Route path='/womens' element={<ShopCategory banner={women_banner} category='women'/>}/>
